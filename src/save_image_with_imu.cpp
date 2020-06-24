66daf6f84dcc28a5c85877836499d6688951cdf2
ozakiryota <kb22056@gmail.com>
2020-06-24 23:30:55 +0900

    updated

--- src/save_image_with_imu.cpp
+++ src/save_image_with_imu.cpp
@@ -14,103 +14,103 @@
 class SaveImageWithIMU{
 	private:
 		/*node hangle*/
-		ros::NodeHandle nh;
-		ros::NodeHandle nhPrivate;
+		ros::NodeHandle _nh;
+		ros::NodeHandle _nhPrivate;
 		/*subscriber*/
-		ros::Subscriber sub_imu;
-		ros::Subscriber sub_image;
-		ros::Subscriber sub_odom;
+		ros::Subscriber _sub_imu;
+		ros::Subscriber _sub_image;
+		ros::Subscriber _sub_odom;
 		/*objects*/
-		sensor_msgs::Imu imu;
-		nav_msgs::Odometry odom_now;
-		nav_msgs::Odometry odom_last;
-		std::ofstream file;
+		sensor_msgs::Imu _imu;
+		nav_msgs::Odometry _odom_now;
+		nav_msgs::Odometry _odom_last;
+		std::ofstream _csvfile;
 		/*counter*/
-		int counter = 0;
+		int _counter = 0;
 		/*flag*/
-		bool got_first_cb_odom = false;
+		bool _got_first_cb_odom = false;
 		/*param*/
-		std::string save_dir_path;
-		std::string save_csv_path;
-		std::string save_img_name;
-		int save_data_limit;
-		double th_diff_position_m;
-		double th_diff_angle_deg;
+		std::string _save_dir_path;
+		std::string _save_csv_path;
+		std::string _save_img_name;
+		int _save_data_limit;
+		double _th_diff_position_m;
+		double _th_diff_angle_deg;
 	public:
 		SaveImageWithIMU();
-		void CallbackIMU(const sensor_msgs::ImuConstPtr& msg);
-		void CallbackImage(const sensor_msgs::ImageConstPtr& msg);
-		void CallbackOdom(const nav_msgs::OdometryConstPtr& msg);
-		bool HasOdomDiff(nav_msgs::Odometry odom1, nav_msgs::Odometry odom2);
-		void Publication(void);
-		void Record(cv_bridge::CvImagePtr cv_ptr);
+		void callbackIMU(const sensor_msgs::ImuConstPtr& msg);
+		void callbackImage(const sensor_msgs::ImageConstPtr& msg);
+		void callbackOdom(const nav_msgs::OdometryConstPtr& msg);
+		bool hasOdomDiff(nav_msgs::Odometry odom1, nav_msgs::Odometry odom2);
+		void publication(void);
+		void record(cv_bridge::CvImagePtr cv_ptr);
 };
 
 SaveImageWithIMU::SaveImageWithIMU()
-	:nhPrivate("~")
+	:_nhPrivate("~")
 {
 	/*param*/
-	nhPrivate.param("save_dir_path", save_dir_path, std::string("dataset"));
-	std::cout << "save_dir_path = " << save_dir_path << std::endl;
-	nhPrivate.param("save_csv_path", save_csv_path, std::string(save_dir_path + "/save_image_with_imu.csv"));
-	std::cout << "save_csv_path = " << save_csv_path << std::endl;
-	nhPrivate.param("save_img_name", save_img_name, std::string("img"));
-	std::cout << "save_img_name = " << save_img_name << std::endl;
-	nhPrivate.param("save_data_limit", save_data_limit, 10);
-	std::cout << "save_data_limit = " << save_data_limit << std::endl;
-	nhPrivate.param("th_diff_position_m", th_diff_position_m, 10.0);
-	std::cout << "th_diff_position_m = " << th_diff_position_m << std::endl;
-	nhPrivate.param("th_diff_angle_deg", th_diff_angle_deg, 30.0);
-	std::cout << "th_diff_angle_deg = " << th_diff_angle_deg << std::endl;
+	_nhPrivate.param("save_dir_path", _save_dir_path, std::string("dataset"));
+	std::cout << "_save_dir_path = " << _save_dir_path << std::endl;
+	_nhPrivate.param("save_csv_path", _save_csv_path, std::string(_save_dir_path + "/save_image_with_imu.csv"));
+	std::cout << "_save_csv_path = " << _save_csv_path << std::endl;
+	_nhPrivate.param("save_img_name", _save_img_name, std::string("img"));
+	std::cout << "_save_img_name = " << _save_img_name << std::endl;
+	_nhPrivate.param("_save_data_limit", _save_data_limit, 10);
+	std::cout << "_save_data_limit = " << _save_data_limit << std::endl;
+	_nhPrivate.param("_th_diff_position_m", _th_diff_position_m, 10.0);
+	std::cout << "_th_diff_position_m = " << _th_diff_position_m << std::endl;
+	_nhPrivate.param("_th_diff_angle_deg", _th_diff_angle_deg, 30.0);
+	std::cout << "_th_diff_angle_deg = " << _th_diff_angle_deg << std::endl;
 
 	/*subscriber*/
-	sub_imu = nh.subscribe("/imu/data", 1, &SaveImageWithIMU::CallbackIMU, this);
-	sub_image = nh.subscribe("/image_raw", 1, &SaveImageWithIMU::CallbackImage, this);
-	sub_odom = nh.subscribe("/odom", 1, &SaveImageWithIMU::CallbackOdom, this);
+	_sub_imu = _nh.subscribe("/imu/data", 1, &SaveImageWithIMU::callbackIMU, this);
+	_sub_image = _nh.subscribe("/image_raw", 1, &SaveImageWithIMU::callbackImage, this);
+	_sub_odom = _nh.subscribe("/odom", 1, &SaveImageWithIMU::callbackOdom, this);
 	/*initialize*/
-	file.open(save_csv_path, std::ios::app);
-	if(!file){
-		std::cout << "Cannot open " << save_csv_path << std::endl;
+	_csvfile.open(_save_csv_path, std::ios::app);
+	if(!_csvfile){
+		std::cout << "Cannot open " << _save_csv_path << std::endl;
 		exit(1);
 	}
 }
 
-void SaveImageWithIMU::CallbackIMU(const sensor_msgs::ImuConstPtr& msg)
+void SaveImageWithIMU::callbackIMU(const sensor_msgs::ImuConstPtr& msg)
 {
-	imu = *msg;
+	_imu = *msg;
 }
 
-void SaveImageWithIMU::CallbackImage(const sensor_msgs::ImageConstPtr& msg)
+void SaveImageWithIMU::callbackImage(const sensor_msgs::ImageConstPtr& msg)
 {
-	if(counter < save_data_limit){
-		if(got_first_cb_odom && HasOdomDiff(odom_now, odom_last)){
+	if(_counter < _save_data_limit){
+		if(_got_first_cb_odom && hasOdomDiff(_odom_now, _odom_last)){
 			try{
 				cv_bridge::CvImagePtr cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
-				Record(cv_ptr);
+				record(cv_ptr);
 			}
 			catch(cv_bridge::Exception& e){
 				ROS_ERROR("cv_bridge exception: %s", e.what());
 			}
 		}
 	}
-	else if(file.is_open()){
-		file.close();
-		std::cout << save_csv_path << " was closed" << std::endl;
+	else if(_csvfile.is_open()){
+		_csvfile.close();
+		std::cout << _save_csv_path << " was closed" << std::endl;
 	}
 
 	/* std::cout << "msg->encoding = " << msg->encoding << std::endl; */
 }
 
-void SaveImageWithIMU::CallbackOdom(const nav_msgs::OdometryConstPtr& msg)
+void SaveImageWithIMU::callbackOdom(const nav_msgs::OdometryConstPtr& msg)
 {
-	odom_now = *msg;
-	if(!got_first_cb_odom){
-		odom_last = *msg;
-		got_first_cb_odom = true;
+	_odom_now = *msg;
+	if(!_got_first_cb_odom){
+		_odom_last = *msg;
+		_got_first_cb_odom = true;
 	}
 }
 
-bool SaveImageWithIMU::HasOdomDiff(nav_msgs::Odometry odom1, nav_msgs::Odometry odom2)
+bool SaveImageWithIMU::hasOdomDiff(nav_msgs::Odometry odom1, nav_msgs::Odometry odom2)
 {
 	/*position*/
 	double dx = odom2.pose.pose.position.x - odom1.pose.pose.position.x;
@@ -135,15 +135,15 @@ bool SaveImageWithIMU::HasOdomDiff(nav_msgs::Odometry odom1, nav_msgs::Odometry 
 	/* 	<< dpitch/M_PI*180.0 << ", " */
 	/* 	<< dyaw/M_PI*180.0 << std::endl; */
 	/*judge*/
-	if(diff_position_m > th_diff_position_m)	return true;
-	if(diff_angle_deg > th_diff_angle_deg)	return true;
+	if(diff_position_m > _th_diff_position_m)	return true;
+	if(diff_angle_deg > _th_diff_angle_deg)	return true;
 	return false;
 }
 
-void SaveImageWithIMU::Record(cv_bridge::CvImagePtr cv_ptr)
+void SaveImageWithIMU::record(cv_bridge::CvImagePtr cv_ptr)
 {
 	/*check*/
-	std::string save_img_path = save_dir_path + "/" + save_img_name + std::to_string(counter) + ".jpg";
+	std::string save_img_path = _save_dir_path + "/" + _save_img_name + std::to_string(_counter) + ".jpg";
 	std::ifstream ifs(save_img_path);
 	if(ifs.is_open()){
 		std::cout << save_img_path << " already exists" << std::endl;
@@ -152,30 +152,30 @@ void SaveImageWithIMU::Record(cv_bridge::CvImagePtr cv_ptr)
 	/*save image*/
 	cv::imwrite(save_img_path, cv_ptr->image);
 	/*record imu*/
-	file 
-		<< imu.linear_acceleration.x << "," 
-		<< imu.linear_acceleration.y << "," 
-		<< imu.linear_acceleration.z << ","
+	_csvfile 
+		<< _imu.linear_acceleration.x << "," 
+		<< _imu.linear_acceleration.y << "," 
+		<< _imu.linear_acceleration.z << ","
 		<< save_img_path << std::endl;
 	/*print*/
 	double r, p, y;
 	tf::Quaternion q;
-	quaternionMsgToTF(odom_now.pose.pose.orientation, q);
+	quaternionMsgToTF(_odom_now.pose.pose.orientation, q);
 	tf::Matrix3x3(q).getRPY(r, p, y);
-	std::cout << counter << " save point: " 
-		<< odom_now.pose.pose.position.x << ", "
-		<< odom_now.pose.pose.position.y << ", "
-		<< odom_now.pose.pose.position.z << ", "
+	std::cout << _counter << " save point: " 
+		<< _odom_now.pose.pose.position.x << ", "
+		<< _odom_now.pose.pose.position.y << ", "
+		<< _odom_now.pose.pose.position.z << ", "
 		<< r/M_PI*180.0 << ", " 
 		<< p/M_PI*180.0 << ", " 
 		<< y/M_PI*180.0 << std::endl;
 	std::cout << "imu acc: " 
-		<< imu.linear_acceleration.x << "," 
-		<< imu.linear_acceleration.y << "," 
-		<< imu.linear_acceleration.z << std::endl;
+		<< _imu.linear_acceleration.x << "," 
+		<< _imu.linear_acceleration.y << "," 
+		<< _imu.linear_acceleration.z << std::endl;
 	/*count*/
-	++counter;
-	odom_last = odom_now;
+	++_counter;
+	_odom_last = _odom_now;
 }
 
 int main(int argc, char** argv)
