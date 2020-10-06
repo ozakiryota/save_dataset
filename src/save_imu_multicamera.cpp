924956574187decef3eb7b523ebd8e337da1fe80
ozakiryota <kb22056@gmail.com>
2020-10-06 19:43:38 +0900

    updated

--- src/save_imu_multicamera.cpp
+++ src/save_imu_multicamera.cpp
@@ -26,6 +26,7 @@ class SaveImageWithIMU{
 			std::string camera_name;
 			bool got_new_image;
 			cv_bridge::CvImagePtr cv_ptr;
+			std::string save_name;
 			std::string save_path;
 		};
 		std::vector<CAMERA> _list_cameras;
@@ -227,8 +228,9 @@ void SaveImageWithIMU::record(void)
 {
 	for(size_t i=0; i<_list_cameras.size(); ++i){
 		/*check*/
-		// _list_cameras[i].save_path = _save_dir_path + "/" + _save_img_name + std::to_string(_record_counter) + "_" + _list_cameras[i].camera_name + ".jpg";
-		_list_cameras[i].save_path = _save_dir_path + "/" + _save_img_name + _list_cameras[i].camera_name + "_" + std::to_string(_record_counter) + ".jpg";
+		_list_cameras[i].save_name = _save_img_name + _list_cameras[i].camera_name + "_" + std::to_string(_record_counter) + ".jpg";
+		_list_cameras[i].save_path = _save_dir_path + "/" + _list_cameras[i].save_name;
+		/*check*/
 		std::ifstream ifs(_list_cameras[i].save_path);
 		if(ifs.is_open()){
 			std::cout << _list_cameras[i].save_path << " already exists" << std::endl;
@@ -242,7 +244,7 @@ void SaveImageWithIMU::record(void)
 		<< _imu.linear_acceleration.x << "," 
 		<< _imu.linear_acceleration.y << "," 
 		<< _imu.linear_acceleration.z;
-	for(size_t i=0; i<_list_cameras.size(); ++i)	_csvfile << "," << _list_cameras[i].save_path;
+	for(size_t i=0; i<_list_cameras.size(); ++i)	_csvfile << "," << _list_cameras[i].save_name;
 	_csvfile << std::endl;
 	/*print*/
 	double r, p, y;
