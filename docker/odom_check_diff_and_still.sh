d98f4b4eb2bc03cb7c4723e5a7cf3df05bb3df80
ozakiryota <kb22056@gmail.com>
2020-10-17 18:19:28 +0900

    updated

--- docker/odom_check_diff_and_still.sh
+++ docker/odom_check_diff_and_still.sh
@@ -3,7 +3,7 @@
 image_name="save_dataset"
 
 xhost +
-nvidia-docker run -it --rm \
+docker run -it --rm \
 	--env="DISPLAY" \
 	--env="QT_X11_NO_MITSHM=1" \
 	--volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
