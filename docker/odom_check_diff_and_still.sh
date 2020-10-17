83b94b3d79ef716f3e5469d001b50c87e9637b10
ozakiryota <kb22056@gmail.com>
2020-10-17 18:10:34 +0900

    updated

---
+++ docker/odom_check_diff_and_still.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+image_name="save_dataset"
+
+xhost +
+nvidia-docker run -it --rm \
+	--env="DISPLAY" \
+	--env="QT_X11_NO_MITSHM=1" \
+	--volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+	--net=host \
+	$image_name:latest \
+	bash -c "source /home/ros_catkin_ws/devel/setup.bash;roslaunch save_dataset odom_check_diff_and_still.launch"
